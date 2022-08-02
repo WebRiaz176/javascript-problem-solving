b5ba528580540013c0eac326709cf9ffa3fe4082
webriaz176 <mohammadriaz2206@gmail.com>
2022-08-02 11:43:40 +0600

    array-problem-solve

---
+++ Array-js/problem-1/prb-1.js
@@ -0,0 +1,23 @@
+var fruits = ['apple', 'banana', 'orange'];
+console.log(fruits);
+
+//find the index of banana
+var elements = fruits.indexOf ('banana');
+console.log(elements);
+
+//replace Banana With Mango
+
+fruits['1'] = 'mango';
+console.log(fruits);
+
+//remove Orange Add watermelon
+
+fruits.pop();
+console.log(fruits);
+
+fruits.push ('watermelon');
+console.log(fruits);
+
+var elements = fruits.pop();
+console.log(elements);
+
