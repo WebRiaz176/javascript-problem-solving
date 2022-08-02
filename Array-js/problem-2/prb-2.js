f36ebe352d828e539327ad7fac14b3142aac9329
webriaz176 <mohammadriaz2206@gmail.com>
2022-08-02 17:44:24 +0600

    array-problem-2

---
+++ Array-js/problem-2/prb-2.js
@@ -0,0 +1,30 @@
+var myScore = 85;
+var tomScore = 66;
+var janeScore = 95;
+var peterScore = 56;
+var jhonScore = 40;
+var totalScore = [85, 66, 95, 56, 40];
+
+
+if (myScore >= 80 && myScore<=100){
+  console.log('I Got A Grade');
+}
+
+if (tomScore >= 60 && tomScore <=79){
+  console.log('Tom Got B Grade');
+}
+
+if (janeScore >= 80 && janeScore <=100){
+  console.log('Jane Got A Grade');
+}
+
+if (peterScore >= 50 && peterScore <=59){
+  console.log('Peter Got C Garde');
+}
+
+if (jhonScore >= 40 && jhonScore <=49){
+  console.log('Jhon got D Grade');
+}
+else{
+  console.log('fail');
+}
